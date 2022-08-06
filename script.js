ee255b3d110ebbdb57cb7464eb65b57ec3526515
Gaehr <95092933+Gaehr@users.noreply.github.com>
2022-08-06 22:42:01 +0200

    Remove prompt to make it work properly. Define function computerPlay out of randomized pick of Array and define variable where the result is saved

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
-let playerSelection = prompt("Game start! What do you choose? Rock, Paper or Scissors?");
+let playerSelection = "";
+
+//prompt("Game start! What do you choose? Rock, Paper or Scissors?"); 
+//Removing the prompt helped solve the 
+
 
 let computerChoose = [
-    'Rock',
-    'Paper',
-    'Scissors'
-];
+    'Rock', 'Paper', 'Scissors'];
+
+
+let computerPlay = Math.floor(Math.random() * computerChoose.length);
+//Fucking error wtf
+
+let computerSelection = computerChoose[computerPlay];
