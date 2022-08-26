9c36bb56fe198c71341dabec88c6a4cba7b773d1
Gaehr <95092933+Gaehr@users.noreply.github.com>
2022-08-26 21:44:11 +0200

    Add variable with prompt for User Input

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 let playerChoice = prompt('Game Start! Choose your attack, Rock, Paper or Scissors?')
 
-function computerPlay(computerChoose)
+
+
+/* function computerPlay(computerChoose)
 {
   
 return computerChoose[Math.floor(Math.random()*computerChoose.length)];
@@ -9,6 +11,8 @@ return computerChoose[Math.floor(Math.random()*computerChoose.length)];
 }
 
 let computerChoose = ['Rock', 'Paper', 'Scissors'];
+*/
+
 console.log(computerPlay(computerChoose));
 
-    
+// let result = window.prompt('You win! Rock beats Scissors!')
